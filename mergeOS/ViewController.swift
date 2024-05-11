7f9284bb6cdfb409b2827fa33a5416464dec3a04
JuditKaramazov <j.lazaromoyano7@gmail.com>
2024-05-11 22:16:21 +0200

    Add ViewController

---
+++ mergeOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2024 Judit Karamazov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Updates the view - if already loaded.
+        }
+    }
+}
+
